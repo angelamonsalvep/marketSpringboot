b4d70bcfdca92b19ade962c7d8ceef0f2b1606cb
Angela Monsalve <angelamonsalve@wposs.com>
2022-01-21 17:04:50 -0500

    Implementación: se crea la interfaz ProductoCrudRepository y la clase ProductoRepository. Se crean metodos para interactuar con la tabla productos de la base de datos.

---
+++ src/main/java/com/angela/market/persistence/ProductoRepository.java
@@ -0,0 +1,40 @@
+package com.angela.market.persistence;
+
+import com.angela.market.persistence.crud.ProductoCrudRepository;
+import com.angela.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+
+}
