e49f4114aca19848fe7ba478d1b4c124abc2fe74
Angela Monsalve <angelamonsalve@wposs.com>
2022-01-21 16:22:51 -0500

    Implementación: Se agrega configuración para mapear las relaciones entre clases, teniendo en cuenta la base de datos

--- src/main/java/com/angela/market/persistence/entity/Producto.java
+++ src/main/java/com/angela/market/persistence/entity/Producto.java
@@ -28,6 +28,13 @@ public class Producto {
 
     private Boolean estado;
 
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    private Categoria categoria;
+
+
+
+
     public Integer getIdProducto() {
         return idProducto;
     }
