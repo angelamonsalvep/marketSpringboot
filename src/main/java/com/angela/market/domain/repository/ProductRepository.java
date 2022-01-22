a83b4fbeae17d947489d14078f9d64c393f618b4
Angela Monsalve <angelamonsalve@wposs.com>
2022-01-22 10:55:42 -0500

    Implementación: creación de la interfaz ProductCrudRepository, para interactuar con los objetos de dominio creados anteriormente

---
+++ src/main/java/com/angela/market/domain/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.angela.market.domain.repository;
+
+import com.angela.market.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    List<Product> getAll();
+    Optional<List<Product>> getByCategoryId (int categoryId);
+    Optional<List<Product>> getScarseProducts (int quantity);
+    Optional<Product> getProduct (int productId);
+    Product save (Product product);
+    void delete (int productId);
+
+}
