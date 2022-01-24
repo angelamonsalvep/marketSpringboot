9717f1d46ad5555d3d39afe416f5b07933390c01
Angela Monsalve <angelamonsalve@wposs.com>
2022-01-24 11:28:37 -0500

    Implementación: Creación de las clases CategoryMapper y ProductMapper para realizar la conversión entre objetos.

---
+++ src/main/java/com/angela/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,30 @@
+package com.angela.market.persistence.mapper;
+
+import com.angela.market.domain.Product;
+import com.angela.market.persistence.entity.Producto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+
+    @Mappings({
+            @Mapping(source = "idproducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "precioventa", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category"),
+    })
+    Product toProduct(Producto producto);
+
+    List<Product> toproducts(List<Producto> productos);
+
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+
+}
