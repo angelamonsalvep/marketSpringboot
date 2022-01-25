0d994674f3602d8ace664872c7bb16d57003c142
Angela Monsalve <angelamonsalve@wposs.com>
2022-01-25 10:49:49 -0500

    Implementación: Se agregan anotaciones para exponer cada servicio de productos.

--- src/main/java/com/angela/market/persistence/ProductoRepository.java
+++ src/main/java/com/angela/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.angela.market.domain.repository.ProductRepository;
 import com.angela.market.persistence.crud.ProductoCrudRepository;
 import com.angela.market.persistence.entity.Producto;
 import com.angela.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +14,11 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
